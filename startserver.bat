71db59515edeed6373e419b39bb0e64827d83c5a
Minecraft小胡 <102411014+minecraftxiaohu@users.noreply.github.com>
2022-03-30 16:51:42 +0800

    Update some files and config

--- startserver.bat
+++ startserver.bat
@@ -1,1 +1,1 @@
-jre1.8.0_321\bin\java.exe -jar forge-1.12.2-14.23.5.2859.jar -Xms1024MB -Xmx1024MB -XX:+AggressiveOpts -XX:+UseG1GC
+jre1.8.0_321\bin\java.exe  -d64 -jar forge-1.12.2-14.23.5.2859.jar -XX:+AggressiveOpts -XX:+UseG1GC -Xmx1024M -Xms1024M 
