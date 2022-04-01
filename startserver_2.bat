552b2a7cd6d22a61010ab75243bc7f8c22471496
Minecraft小胡 <102411014+minecraftxiaohu@users.noreply.github.com>
2022-04-01 08:37:22 +0800

    Update

---
+++ startserver_2.bat
@@ -0,0 +1,4 @@
+@title [NullCraft] Minecraft Server Script
+@echo off
+..\jre1.8.0_321\bin\java.exe -d64 -XX:+AggressiveOpts -XX:+UseG1GC -Xmx1024M -Xms1024M -jar forge-1.12.2-14.23.5.2859.jar nogui
+PAUSE
