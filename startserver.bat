70ad7357b09cbd77031304d6c569064bebfcb493
Minecraft小胡 <102411014+minecraftxiaohu@users.noreply.github.com>
2022-04-09 11:24:22 +0800

    Add files via upload

--- startserver.bat
+++ startserver.bat
@@ -1,1 +1,1 @@
-..\jre1.8.0_321\bin\java.exe  -d64 -jar forge-1.12.2-14.23.5.2859.jar -XX:+AggressiveOpts -XX:+UseG1GC -Xmx1024M -Xms1024M 
+..\jre1.8.0_321\bin\java.exe  -d64 -javaagent:authlib-injector-1.1.42.jar=https://littleskin.cn/api/yggdrasil -jar forge-1.12.2-14.23.5.2859.jar -XX:+AggressiveOpts -XX:+UseG1GC -Xmx1024M -Xms1024M 
--- startserver_2.bat
+++ startserver_2.bat
@@ -1,4 +1,4 @@
 @title [NullCraft] Minecraft Server Script
 @echo off
-..\jre1.8.0_321\bin\java.exe -d64 -XX:+AggressiveOpts -XX:+UseG1GC -Xmx1024M -Xms1024M -jar forge-1.12.2-14.23.5.2859.jar nogui
+..\jre1.8.0_321\bin\java.exe -d64 -javaagent:authlib-injector-1.1.42.jar=https://littleskin.cn/api/yggdrasil -XX:+AggressiveOpts -XX:+UseG1GC -Xmx1024M -Xms1024M -jar forge-1.12.2-14.23.5.2859.jar nogui
 PAUSE
